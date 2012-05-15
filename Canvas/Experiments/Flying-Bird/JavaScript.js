015ff9f71bd299360e078eaa19361f13a70ad19b
Muaz Khan <muazkh@gmail.com>
2012-05-15 16:27:09 +0500

    updated!

--- Canvas/Experiments/Flying-Bird/JavaScript.js
+++ Canvas/Experiments/Flying-Bird/JavaScript.js
@@ -1,4 +1,5 @@
-﻿(function() {
+﻿(function () { var lastTime = 0, vendors = ['ms', 'moz', 'webkit', 'o']; for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) { window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame']; window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'RequestCancelAnimationFrame']; } if (!window.requestAnimationFrame) window.requestAnimationFrame = function (callback, element) { var currTime = new Date().getTime(); var timeToCall = Math.max(0, 16 - (currTime - lastTime)); var id = window.setTimeout(function () { callback(currTime + timeToCall); }, timeToCall); lastTime = currTime + timeToCall; return id; }; if (!window.cancelAnimationFrame) window.cancelAnimationFrame = function (id) { clearTimeout(id); }; }());                
+(function() {
     
     var canvas = document.getElementById('flying-bird'), 
     context = canvas.getContext('2d');
@@ -84,7 +85,7 @@
             }
             
             if (x > 0)
-                webkitRequestAnimationFrame(drawBird);
+                requestAnimationFrame(drawBird);
         
         }
 
