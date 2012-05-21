0d62617258751c39d984474c2d5233f83f4ba7b0
Muaz Khan <muazkh@gmail.com>
2012-05-21 14:07:29 +0500

    Updated Flying Bird Experiment: http://muaz-khan.github.com/Everything/Canvas/Experiments/Flying-Bird/

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,26 @@
-﻿/*
-(function () {
+﻿(function () {
+    /*≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡*/
     if (!document.createElement('canvas').getContext) {
-        //document.body.innerHTML = '<h1 style="height:auto;color:red;font-size:30px;">You\'re Old!<br /><br />Old is not Gold!<br /><br />Target feature has no support!</h1>';
-        alert('Target feature is not supported on your web-browser!');
-    }
-
+        document.body.innerHTML = '<h1 style="height:auto;color:red;font-size:30px;">Excuse me Sir,<br /><br />You are using very old web-browser!<br /><br />Please upgrade it.</h1>';        
+    }    
+    /*≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡*/
+    var prepend = function (parent, elementToPrepend)
+    {
+        return parent.insertBefore(elementToPrepend, parent.firstChild);
+    };
+    
+    /*≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡*/
+    var div = document.createElement('div');
+    
+    div.innerHTML = '<g:plusone></g:plusone>';
+    div.className = 'gplus-button';
+    
+    var body = document.body;
+    
+    if(body.insertBefore) prepend(body, div);
+    else document.body.appendChild(div);    
+    /*≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡*/
     // GPlus Button 
-    //var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);    
+    /*≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡*/
 })();
-
-*/
